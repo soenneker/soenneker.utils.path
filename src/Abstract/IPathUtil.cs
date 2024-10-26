57d42ac5b04ff655638043fcff2f959b6a67d11d
Jake Soenneker <jake@soenneker.com>
2024-10-25 20:14:27 -0500

    New method

--- src/Abstract/IPathUtil.cs
+++ src/Abstract/IPathUtil.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics.Contracts;
 
 namespace Soenneker.Utils.Path.Abstract;
 
@@ -7,4 +10,13 @@ namespace Soenneker.Utils.Path.Abstract;
 /// </summary>
 public interface IPathUtil
 {
+    /// <summary>
+    /// Asynchronously generates a unique, thread-safe file path within the specified directory for a given URI.
+    /// </summary>
+    /// <param name="directory">The directory in which the unique file path will be created.</param>
+    /// <param name="uri">The URI of the file to be saved, used to derive the file name.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task that represents the asynchronous operation, containing the unique file path as a <see cref="string"/>.</returns>
+    [Pure]
+    ValueTask<string> GetThreadSafeUniqueFilePath(string directory, string uri, CancellationToken cancellationToken);
 }
--- src/PathUtil.cs
+++ src/PathUtil.cs
@@ -1,12 +1,17 @@
 using Soenneker.Utils.Path.Abstract;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Nito.AsyncEx;
 
 namespace Soenneker.Utils.Path;
 
 /// <inheritdoc cref="IPathUtil"/>
 public class PathUtil: IPathUtil
 {
+    private readonly Lazy<AsyncLock> _asyncLock = new();
+
     public PathUtil()
     {
     }
@@ -23,5 +28,30 @@ public class PathUtil: IPathUtil
             .LastOrDefault();
 
         return lastPathSegment;
+    }
+
+    public async ValueTask<string> GetThreadSafeUniqueFilePath(string directory, string uri, CancellationToken cancellationToken)
+    {
+        string fileName = System.IO.Path.GetFileName(new Uri(uri).AbsolutePath);
+        string filePath = System.IO.Path.Combine(directory, fileName);
+        int count = 1;
+        string fileExtension = System.IO.Path.GetExtension(filePath);
+        string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(filePath);
+
+        while (true)
+        {
+            string tempFilePath = filePath;
+
+            using (await _asyncLock.Value.LockAsync(cancellationToken).ConfigureAwait(false))
+            {
+                if (!System.IO.File.Exists(tempFilePath))
+                {
+                    return tempFilePath;
+                }
+
+                string tempFileName = $"{fileNameWithoutExtension}({count++}){fileExtension}";
+                filePath = System.IO.Path.Combine(directory, tempFileName);
+            }
+        }
     }
 }
