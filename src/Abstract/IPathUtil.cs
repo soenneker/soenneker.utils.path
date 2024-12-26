fbef14945d4c8d3c63e62e9600fd55abe8ed2357
Jake Soenneker <jake@soenneker.com>
2024-12-25 19:22:36 -0600

    New methods

--- src/Abstract/IPathUtil.cs
+++ src/Abstract/IPathUtil.cs
@@ -11,12 +11,29 @@ namespace Soenneker.Utils.Path.Abstract;
 public interface IPathUtil
 {
     /// <summary>
-    /// Asynchronously generates a unique, thread-safe file path within the specified directory for a given URI.
+    /// Generates a unique file path based on a specified directory and URI. 
+    /// If a file with the same name exists, a numeric suffix is appended to the file name to ensure uniqueness.
     /// </summary>
-    /// <param name="directory">The directory in which the unique file path will be created.</param>
-    /// <param name="uri">The URI of the file to be saved, used to derive the file name.</param>
+    /// <param name="directory">The directory where the file path should be generated.</param>
+    /// <param name="uri">The URI used to extract the file name.</param>
     /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
-    /// <returns>A task that represents the asynchronous operation, containing the unique file path as a <see cref="string"/>.</returns>
-    [Pure]
-    ValueTask<string> GetThreadSafeUniqueFilePath(string directory, string uri, CancellationToken cancellationToken);
+    /// <returns>A unique file path in the specified directory.</returns>
+    ValueTask<string> GetThreadSafeUniqueFilePath(string directory, string uri, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Generates a random, unique file path in a specified directory with a given file extension.
+    /// </summary>
+    /// <param name="directory">The directory where the file path should be generated.</param>
+    /// <param name="fileExtension">The desired file extension (e.g., ".txt").</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A unique file path in the specified directory with the specified file extension.</returns>
+    ValueTask<string> GetThreadSafeRandomUniqueFilePath(string directory, string fileExtension, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Generates a random, unique file path in the system's temporary storage directory with a given file extension.
+    /// </summary>
+    /// <param name="fileExtension">The desired file extension (e.g., ".tmp").</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A unique file path in the system's temporary directory with the specified file extension.</returns>
+    ValueTask<string> GetThreadSafeTempUniqueFilePath(string fileExtension, CancellationToken cancellationToken = default);
 }
--- src/PathUtil.cs
+++ src/PathUtil.cs
@@ -4,37 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
 using Nito.AsyncEx;
+using System.IO;
+using System.Diagnostics.Contracts;
 
 namespace Soenneker.Utils.Path;
 
 /// <inheritdoc cref="IPathUtil"/>
-public class PathUtil: IPathUtil
+public class PathUtil : IPathUtil
 {
     private readonly Lazy<AsyncLock> _asyncLock = new();
 
-    public PathUtil()
-    {
-    }
-
     /// <summary>
-    /// Retrieves the last segment of a file or directory path. OS agnostic.
+    /// Retrieves the last segment of a file or directory path. OS-agnostic.
     /// </summary>
     /// <param name="path">The path to process.</param>
     /// <returns>The last segment of the path, or null if the path is empty.</returns>
+    [Pure]
     public static string? GetLastPathSegment(string path)
     {
         string? lastPathSegment = path
-            .Split(new[] { System.IO.Path.DirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries)
+            .Split([System.IO.Path.DirectorySeparatorChar], StringSplitOptions.RemoveEmptyEntries)
             .LastOrDefault();
 
         return lastPathSegment;
     }
 
-    public async ValueTask<string> GetThreadSafeUniqueFilePath(string directory, string uri, CancellationToken cancellationToken)
+    public async ValueTask<string> GetThreadSafeUniqueFilePath(string directory, string uri, CancellationToken cancellationToken = default)
     {
         string fileName = System.IO.Path.GetFileName(new Uri(uri).AbsolutePath);
         string filePath = System.IO.Path.Combine(directory, fileName);
-        int count = 1;
+        var count = 1;
         string fileExtension = System.IO.Path.GetExtension(filePath);
         string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(filePath);
 
@@ -44,13 +43,50 @@ public class PathUtil: IPathUtil
 
             using (await _asyncLock.Value.LockAsync(cancellationToken).ConfigureAwait(false))
             {
-                if (!System.IO.File.Exists(tempFilePath))
-                {
+                if (!File.Exists(tempFilePath))
                     return tempFilePath;
-                }
 
                 string tempFileName = $"{fileNameWithoutExtension}({count++}){fileExtension}";
                 filePath = System.IO.Path.Combine(directory, tempFileName);
+            }
+        }
+    }
+
+    public async ValueTask<string> GetThreadSafeRandomUniqueFilePath(string directory, string fileExtension, CancellationToken cancellationToken = default)
+    {
+        if (!fileExtension.StartsWith('.'))
+            fileExtension = '.' + fileExtension;
+
+        while (true)
+        {
+            var randomFileName = $"{Guid.NewGuid()}{fileExtension}";
+            string filePath = System.IO.Path.Combine(directory, randomFileName);
+
+            using (await _asyncLock.Value.LockAsync(cancellationToken).ConfigureAwait(false))
+            {
+                // Check if the file path already exists
+                if (!File.Exists(filePath))
+                    return filePath;
+            }
+        }
+    }
+
+    public async ValueTask<string> GetThreadSafeTempUniqueFilePath(string fileExtension, CancellationToken cancellationToken = default)
+    {
+        if (!fileExtension.StartsWith('.'))
+            fileExtension = '.' + fileExtension;
+
+        string tempDirectory = System.IO.Path.GetTempPath();
+
+        while (true)
+        {
+            var randomFileName = $"{Guid.NewGuid()}{fileExtension}";
+            string filePath = System.IO.Path.Combine(tempDirectory, randomFileName);
+
+            using (await _asyncLock.Value.LockAsync(cancellationToken).ConfigureAwait(false))
+            {
+                if (!File.Exists(filePath))
+                    return filePath;
             }
         }
     }
