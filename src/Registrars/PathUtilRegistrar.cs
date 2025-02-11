0807ef188b8c3c0e950f70f725ba271b748300dd
Jake Soenneker <jake@soenneker.com>
2025-02-11 13:01:27 -0600

    Fluent

--- src/Registrars/PathUtilRegistrar.cs
+++ src/Registrars/PathUtilRegistrar.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Soenneker.Utils.Path.Abstract;
 
@@ -12,16 +12,18 @@ public static class PathUtilRegistrar
     /// <summary>
     /// Adds <see cref="IPathUtil"/> as a singleton service. <para/>
     /// </summary>
-    public static void AddPathUtilAsSingleton(this IServiceCollection services)
+    public static IServiceCollection AddPathUtilAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<IPathUtil, PathUtil>();
+        return services;
     }
 
     /// <summary>
     /// Adds <see cref="IPathUtil"/> as a scoped service. <para/>
     /// </summary>
-    public static void AddPathUtilAsScoped(this IServiceCollection services)
+    public static IServiceCollection AddPathUtilAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<IPathUtil, PathUtil>();
+        return services;
     }
 }
